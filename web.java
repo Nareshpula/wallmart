abddb493d8f1cb0db7f7f681a3ae068e065c6234
nareshroyal <pulanareshroyal@gmail.com>
2017-05-10 01:15:09 -0700

    i commited by the something changes by the web file

--- web.java
+++ web.java
@@ -1,3 +1,4 @@
 is the small file to carry of the new things they can easy to mentioned by the option.
 it has to pay the loat of mony to build and release engineer.
 they can easly understand to the latest problem to the option.
+XXX = 520.
