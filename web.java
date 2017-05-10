0145eaab026b434a25ef9401f727ff25d7f02f31
nareshroyal <pulanareshroyal@gmail.com>
2017-05-10 00:47:07 -0700

    i am commite by the new file called web.java

---
+++ web.java
@@ -0,0 +1,3 @@
+is the small file to carry of the new things they can easy to mentioned by the option.
+it has to pay the loat of mony to build and release engineer.
+they can easly understand to the latest problem to the option.
