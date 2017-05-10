bfe86db443f0013c327f234e41700d1977834793
nareshroyal <pulanareshroyal@gmail.com>
2017-05-10 00:16:16 -0700

    i am commited to the new file called power.java file

---
+++ power.java
@@ -0,0 +1,4 @@
+it is the file to create to the maintained by the new thing.
+it has to pay the maintain this file.
+i feel tommarow i have capgemini intrview.
+i denfinetly selected that company by god grace.
